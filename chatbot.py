5bfccb1b37457d766e8295b193821ece6d23bff4
uprogr <uprogr@gmail.com>
2025-04-29 11:16:26 +0800

    asdasd

--- chatbot.py
+++ chatbot.py
@@ -2,4 +2,4 @@ from app import create_app, socketio
 
 if __name__ == "__main__":
     app = create_app()
-    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=False)
